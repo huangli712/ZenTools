36ccac1cb1fd8885b1e2c8cc627fb71fe4feee6e
Li Huang <lihuang.dmft@gmail.com>
2021-11-07 23:17:35 +0800

    add band4vasp.jl

---
+++ src/band4vasp.jl
@@ -0,0 +1,21 @@
+#!/usr/bin/env julia
+
+#
+# Project : Begonia
+# Source  : band4vasp.jl
+# Author  : Li Huang (lihuang.dmft@gmail.com)
+# Status  : Testing
+#
+# Last modified: 2021/11/07
+#
+
+# Update LOAD_PATH
+push!(LOAD_PATH, ENV["ZEN_CORE"])
+
+# Using standard library
+using Printf
+
+# Using the ZenCore library
+using ZenCore
+
+println("hello world")
