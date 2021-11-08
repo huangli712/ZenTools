71600a6e8bfeddf961537696acce508f93dc6132
Li Huang <lihuang.dmft@gmail.com>
2021-11-08 21:34:55 +0800

    update wannband.jl

--- src/wannband.jl
+++ src/wannband.jl
@@ -45,6 +45,9 @@ kend   = [0.5 0.0 0.0; # X
 # Generate 𝑘-list
 kpath, xpath = w90_make_kpath(ndiv, kstart, kend)
 
+# Get an uniform 𝑘-mesh
+kmesh, weight = qeio_kmesh("dft")
+
 # Determine the fermi level
 fermi = qeio_fermi("dft", false)
 
@@ -52,17 +55,33 @@ fermi = qeio_fermi("dft", false)
 rdeg, rvec, hamr = w90_read_hamr("dft")
 
 # Build H(𝑘) along high-symmetry directions
-println("Generate H(K)")
+println("Generate H(𝑘) where 𝑘 along high-symmetry directions")
 hamk = w90_make_hamk(kpath, rdeg, rvec, hamr)
 
 # Calculate the band structures
-println("Diagonalize H(K)")
+println("Diagonalize H(𝑘) where 𝑘 along high-symmetry directions")
 eigs, evec = w90_diag_hamk(hamk)
-nband, nkpt = size(eigs)
+
+# Build H(𝑘) in an uniform 𝑘-mesh
+println("Generate H(𝑘) in an uniform 𝑘-mesh")
+hamk = w90_make_hamk(kmesh, rdeg, rvec, hamr)
+
+# Perform 𝑘-summation to calculate band levels
+println("Try to evaluate the band levels")
+nwann, _, nkpt = size(hamk)
+level = zeros(C64, nwann)
+for k = 1:nkpt
+    for b = 1:nwann
+        level[b] = level[b] + hamk[b,b,k] * weight[k]
+    end
+end
+@. level = level / nkpt
+@show level .- fermi
 
 # Dump the band structures
 println("Dump band structures into band.dat")
 open("band.dat", "w") do fout
+    nband, nkpt = size(eigs)
     for b = 1:nband
         for k = 1:nkpt
             println(fout, xpath[k], " ", eigs[b,k] - fermi)
@@ -73,6 +92,7 @@ end
 
 # Dump the 𝑘-list
 open("kpath.dat", "w") do fout
+    nband, nkpt = size(eigs)
     for k = 1:nkpt
         @printf(fout, "%12.6f %8.6f %8.6f %6.4f\n", kpath[k,:]..., 1.00)
     end
